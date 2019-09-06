5c2a2ae548d01740e10a7d424a5a98745cabf8bc
Sedrick Cashaw Jr <scashaw@umich.edu>
2019-09-05 21:02:42 -0700

    initial project setup

---
+++ App-Store/App-Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App-Store
+//
+//  Created by Sedrick Cashaw Jr on 9/5/19.
+//  Copyright © 2019 Sedrick Cashaw Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
